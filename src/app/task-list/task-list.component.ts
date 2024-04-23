2f77ccd3e0cbf937c9f95fc14917d8401ec9674d
Aubry Thomas LOUVET <129608889+AubryLvt@users.noreply.github.com>
2024-04-23 18:28:32 +0200

    task-list.component.ts implementation 1

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
   imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
+  componentTasks: Task[];
 
+  constructor(private taskServiceVar: TaskService) {}
 }
