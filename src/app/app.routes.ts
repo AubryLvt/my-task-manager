ed46ffa1934ecd8f8cc83ed84ce88c609ac98817
Aubry Thomas LOUVET <129608889+AubryLvt@users.noreply.github.com>
2024-04-23 19:03:18 +0200

    app.routes.ts implementation
    task-list works !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'task-list', component: TaskListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
