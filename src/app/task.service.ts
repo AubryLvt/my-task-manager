a9be150a4b3e5b2b782215800cc63f1de837ecad
Aubry Thomas LOUVET <129608889+AubryLvt@users.noreply.github.com>
2024-04-23 18:21:14 +0200

    task.service.ts refactorisation et method saveTasks

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -9,52 +9,53 @@ import { Task } from './task.model';
 export class TaskService {
   // Clé utilisée pour stocker les tâches dans localStorage
   private readonly storageKey = 'tasks';
+  // Variable stockant les tâches :
+  private tasks: Task[];
 
-  constructor() {}
-
-  // --------------------------------------
-  // Méthode pour ajouter une nouvelle tâche
-  // --------------------------------------
-  addTask(task: Task): void {
-    // Récupérer les tâches existantes depuis localStorage
-    let tasks: Task[] = JSON.parse(localStorage.getItem(this.storageKey)) || [];
-    // Ajouter la nouvelle tâche à la liste
-    tasks.push(task);
-    // Sauvegarder la liste mise à jour dans localStorage
-    localStorage.setItem(this.storageKey, JSON.stringify(tasks));
+  constructor() {
+    this.tasks = this.getStoredTasks();
+    // permet d'initialiser l'instance de TaskService
+    // sinon le service serait vide lors du lancement
   }
 
-  // --------------------------------------
-  // Méthode pour récupérer toutes les tâches
-  // --------------------------------------
-  getTasks(): Task[] {
+  // -------- Méthodes pour récupérer toutes les tâches --------
+  private getStoredTasks(): Task[] {
     // Récupérer les tâches depuis localStorage, ou retourner un tableau vide si aucune tâche n'est trouvée
     return JSON.parse(localStorage.getItem(this.storageKey)) || [];
   }
 
-  // --------------------------------------
-  // Méthode pour marquer une tâche comme complétée
-  // --------------------------------------
+  getTasks(): Task[] {
+    return this.getStoredTasks();
+  }
+
+  // -------- Méthode pour ajouter une nouvelle tâche --------
+  addTask(task: Task): void {
+    // Ajouter la nouvelle tâche à la liste
+    this.tasks.push(task);
+    // Sauvegarder la liste mise à jour dans localStorage
+    localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+  }
+
+  // -------- Méthode pour sauvegarder les tâches dans localStorage --------
+  private saveTasks(): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+  }
+
+  // -------- Méthode pour marquer une tâche comme complétée --------
   completeTask(taskId: number): void {
-    // Récupérer les tâches existantes depuis localStorage
-    let tasks: Task[] = JSON.parse(localStorage.getItem(this.storageKey)) || [];
     // Trouver l'index de la tâche à compléter dans la liste des tâches
-    let taskIndex = tasks.findIndex((task) => task.id === taskId);
+    let taskIndex = this.tasks.findIndex((task) => task.id === taskId);
     // Si la tâche est trouvée, la marquer comme complétée et sauvegarder la liste mise à jour dans localStorage
     if (taskIndex !== -1) {
-      tasks[taskIndex].completed = true;
-      localStorage.setItem(this.storageKey, JSON.stringify(tasks));
+      this.tasks[taskIndex].completed = true;
+      localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
     }
   }
 
-  // --------------------------------------
-  // Méthode pour supprimer une tâche
-  // --------------------------------------
+  // -------- Méthode pour supprimer une tâche --------
   deleteTask(taskId: number): void {
-    // Récupérer les tâches existantes depuis localStorage
-    let tasks: Task[] = JSON.parse(localStorage.getItem(this.storageKey)) || [];
     // Filtrer les tâches pour exclure celle à supprimer
-    let updatedTasks = tasks.filter((task) => task.id !== taskId);
+    let updatedTasks = this.tasks.filter((task) => task.id !== taskId);
     // Sauvegarder la liste mise à jour dans localStorage
     localStorage.setItem(this.storageKey, JSON.stringify(updatedTasks));
   }
